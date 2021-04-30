096a802ec83363a990a30e3c463869710eda1882
MohamadKazemiRe9 <mohammadkazemire9@gmail.com>
2021-04-30 21:28:15 +0430

    settings

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,10 +1,12 @@
 import sys
 import pygame
+from settings import Settings
 
 class AlienInvasion:
     def __init__(self):
         pygame.init()
-        self.screen = pygame.display.set_mode((1200,800))
+        self.settings = Settings()
+        self.screen = pygame.display.set_mode((self.settings.screen_width,self.settings.screen_height))
         pygame.display.set_caption('Alien Invasion')
 
     def run_game(self):
@@ -12,6 +14,8 @@ class AlienInvasion:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
+
+            self.screen.fill(self.settings.bg_color)
 
             pygame.display.flip()
 
---
+++ settings.py
@@ -0,0 +1,5 @@
+class Settings:
+    def __init__(self):
+        self.bg_color = (14,3,20)
+        self.screen_width = 1200
+        self.screen_height = 800
